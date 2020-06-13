2bfaa04869f5deb2557f93074ea0e3f50158c9df
Andy Meneely <andy@se.rit.edu>
2020-06-13 01:09:35 -0400

    spec for migration generator

--- spec/migrate/migration_generator_spec.rb
+++ spec/migrate/migration_generator_spec.rb
@@ -1,7 +1,15 @@
 require_relative '../helper'
 
 describe VHP::MigrationGenerator do
+
   context :run do
-    it 'does something... '
+
+    it 'calls FileUtils' do
+      expect(FileUtils).to receive(:cp).with(/migration_template/, /foobar/)
+      silently do
+        VHP::MigrationGenerator.new.run('foobar')
+      end
+    end
+
   end
 end
